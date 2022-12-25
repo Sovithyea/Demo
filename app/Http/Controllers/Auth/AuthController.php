d2e1b332ebcf4f261b545d918461b8bd3e54f927
Sovithyea <sovithyeativ@gmail.com>
2022-12-25 11:51:40 +0700

    setup auth

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login()
+    {
+        return view('auth.login');
+    }
+
+    public function storeLogin(LoginRequest $request)
+    {
+        if(Auth::attempt($request->validated())) {
+            $request->session()->regenerate();
+            return to_route('dashboard');
+        }
+        return back();
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return to_route('login');
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\BrandController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ColorController;
@@ -20,24 +21,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('dashboard');
+Route::get('login', [AuthController::class, 'login'])->name('login');
+Route::post('login', [AuthController::class, 'storeLogin'])->name('store-login');
+
+Route::middleware('auth')->group(function() {
+
+    Route::get('logout', [AuthController::class, 'logout'])->name('logout');
+
+    Route::get('/', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::resource('product-types', ProductTypeController::class);
+    Route::resource('suppliers', SupplierController::class);
+    Route::resource('brands', BrandController::class);
+    Route::resource('modells', ModellController::class);
+    Route::resource('categories', CategoryController::class);
+    Route::resource('posts', PostController::class);
+    Route::resource('colors', ColorController::class);
+
+
+    //soft_delete
+    Route::get('product-types-trash', [ProductTypeController::class, 'trash'])->name('product-types.trash');
+    Route::get('product-types/{product_type}/recovery', [ProductTypeController::class, 'recovery'])->name('product-types.recovery');
+    Route::get('product-types/{product_type}/forceDelete', [ProductTypeController::class, 'forceDelete'])->name('product-types.force-delete');
+
 });
 
-// Route::get('/colors', function () {
-//     return view('color.create');
-// });
-
-Route::resource('colors', ColorController::class);
-Route::resource('product-types', ProductTypeController::class);
-Route::resource('suppliers', SupplierController::class);
-Route::resource('brands', BrandController::class);
-Route::resource('modells', ModellController::class);
-Route::resource('categories', CategoryController::class);
-Route::resource('posts', PostController::class);
-
-
-//soft_delete
-Route::get('product-types-trash', [ProductTypeController::class, 'trash'])->name('product-types.trash');
-Route::get('product-types/{product_type}/recovery', [ProductTypeController::class, 'recovery'])->name('product-types.recovery');
-Route::get('product-types/{product_type}/forceDelete', [ProductTypeController::class, 'forceDelete'])->name('product-types.force-delete');
